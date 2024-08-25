23a787306897b7e46661b1bdc34aceee47b1ea34
ashis <ashisrout541@gmail.com>
2024-08-25 20:36:12 +0530

    refresh token added

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,14 +1,17 @@
 
 import { Router } from 'express';
 import {verifyJWT} from '../middlewares/auth.middleware.js';
-import { registerUser,loginUser,logoutUser } from '../controllers/user.controller.js';
+import { registerUser,loginUser,logoutUser,refreshAccessToken } from '../controllers/user.controller.js';
 
 const routes = new Router();
 
 
 routes.route("/register").post(registerUser)
 routes.route('/login').post(loginUser)
+
+//secure routes
 routes.route('/logout').post(verifyJWT,logoutUser)
+routes.route('/refreshtoken').post(refreshAccessToken)
 
 
 
