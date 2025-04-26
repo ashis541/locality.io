a0841173015115913a1a19219b95b33604ef39eb
ashis <ashisrout541@gmail.com>
2025-04-26 21:07:01 +0530

    form dara accept

--- src/controllers/organization.controller.js
+++ src/controllers/organization.controller.js
@@ -80,7 +80,7 @@ const loginUser = asyncHandler(async (req, res) => {
   });
 
   if (!user) {
-    throw new ApiError(404, "User does not exist");
+    throw new ApiError(200, "User does not exist");
   }
 
   const isPasswordValid = await user.isPasswordCorrect(password);
--- src/routes/organization.routes.js
+++ src/routes/organization.routes.js
@@ -2,12 +2,15 @@
 import { Router } from 'express';
 import {verifyJWT} from '../middlewares/auth.middleware.js';
 import { registerUser,loginUser,logoutUser,refreshAccessToken } from '../controllers/organization.controller.js';
+import multer from 'multer'
 
 const routes = new Router();
+const upload = multer()
 
 
 routes.route("/register").post(registerUser)
-routes.route('/login').post(loginUser)
+//used to access form data in the body of the request
+routes.route('/login').post(upload.none(),loginUser)
 //secure routes
 routes.route('/logout').post(verifyJWT,logoutUser)
 routes.route('/refreshtoken').post(refreshAccessToken)
